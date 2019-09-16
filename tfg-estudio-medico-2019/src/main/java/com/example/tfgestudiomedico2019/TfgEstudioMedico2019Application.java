c4e15d1cf4f8eb20afcb3766c58a05c803e2c8cd
myscel <edugon04@ucm.es>
2019-09-16 09:22:10 +0200

    First Project Hola Mundo

---
+++ tfg-estudio-medico-2019/src/main/java/com/example/tfgestudiomedico2019/TfgEstudioMedico2019Application.java
@@ -0,0 +1,13 @@
+package com.example.tfgestudiomedico2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgEstudioMedico2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgEstudioMedico2019Application.class, args);
+	}
+
+}
