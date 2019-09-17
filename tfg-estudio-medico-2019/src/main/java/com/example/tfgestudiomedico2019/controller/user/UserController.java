f35e1b04eae98253959c826c0a29cfe647d14369
myscel <edugon04@ucm.es>
2019-09-17 14:46:44 +0200

    CrossOrigin Implemented to connect with FRONT

--- tfg-estudio-medico-2019/src/main/java/com/example/tfgestudiomedico2019/controller/user/UserController.java
+++ tfg-estudio-medico-2019/src/main/java/com/example/tfgestudiomedico2019/controller/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.tfgestudiomedico2019.controller.user;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.tfgestudiomedico2019.model.rest.ResponseDto;
 import com.example.tfgestudiomedico2019.model.rest.UserDto;
 
+
 @RequestMapping("/user")
 public interface UserController {
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<ResponseDto> loginUser(@RequestBody UserDto userDto);
 
--- tfg-estudio-medico-2019/src/main/java/com/example/tfgestudiomedico2019/controller/user/UserControllerImpl.java
+++ tfg-estudio-medico-2019/src/main/java/com/example/tfgestudiomedico2019/controller/user/UserControllerImpl.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.tfgestudiomedico2019.business.user.UserBusiness;
@@ -18,8 +19,10 @@ public class UserControllerImpl implements UserController {
 	private UserBusiness userBusiness;
 	
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@Override
 	public ResponseEntity<ResponseDto> loginUser(UserDto userDto) {
+		System.out.println(userDto);
 		
 		ModelMapper mapper = new ModelMapper();
 		
